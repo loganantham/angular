e5f419334cffa9a6b67d42d0ed20e9192c084bcb
Loganantham <logananthamloga@gmail.com>
2019-11-03 20:55:50 +0530

    Topmenu added

--- app-ui/src/app/login/login.component.ts
+++ app-ui/src/app/login/login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from '../service/login.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor(private loginService : LoginService,private router : Router) { }
 
   ngOnInit() {
+    
+  }
+
+  login(){
+    this.router.navigate(['home']);
   }
 
 }
